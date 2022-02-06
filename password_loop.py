02e3c9aa4b30858dc957dc5b6b9e07b2aad6c863
Kiwe <kiwe0410@gmail.com>
2022-02-06 19:50:15 +0800

    password

---
+++ password_loop.py
@@ -0,0 +1,28 @@
+#輸入密碼程式
+#while True:
+
+#while True:
+#    password = input('請輸入密碼: ')
+#    if password != 'a1234567':
+#        print('密碼錯誤還有兩次機會')
+#    elif password != 'a1234567':
+#        print('密碼錯誤還有一次機會')
+#    elif password == 'a1234567':
+#        print('登入成功')
+#        break
+
+password = 'a1234567'
+i = 3 #剩餘機會
+while True:
+    pwd = input("請輸入密碼: ")
+    if pwd == password:
+        print('登入成功')
+        break #逃出迴圈
+    else:
+        i = i - 1
+        print('密碼錯誤! 還有', i, '次機會')
+        if i == 0:
+            break
+    
+        
+        
